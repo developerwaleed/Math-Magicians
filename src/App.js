4f4363f5b5009085a549d1cc3879c3ff068911b3
Waleed Amjad <waleedamjad99@hotmail.com>
2022-06-28 23:11:23 +0500

    Applied Suggestions Thanks to ShadyShawkat ;)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-// const calculator = new Calculator();
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-function App() {
-  return (
-    <>
-      <Calculator />
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
 }
-
-export default App;
